7168c47636a86e856523bbb3ef49e9b21a7b95fc
emplace_hack <mike.wellner.private@gmail.com>
2022-06-28 22:17:05 +0200

    Disabled tooltip interactivity

--- navigation-drawer.module.ts
+++ navigation-drawer.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTooltipModule, MatTooltipDefaultOptions, MAT_TOOLTIP_DEFAULT_OPTIONS } from '@angular/material/tooltip';
 
 import { NavigationDrawerItemComponent } from './navigation-drawer-item/navigation-drawer-item.component';
 import { NavigationDrawerComponent } from './navigation-drawer/navigation-drawer.component';
+
+export const otherOptions: MatTooltipDefaultOptions = {
+  showDelay: 0,
+  hideDelay: 0,
+  touchendHideDelay: 0,
+  disableTooltipInteractivity: true
+};
+
 
 @NgModule({
   imports: [
@@ -19,7 +27,10 @@ import { NavigationDrawerComponent } from './navigation-drawer/navigation-drawer
   exports: [
     NavigationDrawerItemComponent,
     NavigationDrawerComponent
-  ]
+  ],
+  providers: [
+    { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: otherOptions }
+  ],
 })
 
 export class NavigationDrawerModule {}
